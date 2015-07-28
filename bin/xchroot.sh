03a48b2abd71661554dd7c2d123c45d3cec92de8
Israel <i@tiamat>
2015-07-28 22:59:35 +0300

    More specific mount options.

--- bin/xchroot.sh
+++ bin/xchroot.sh
@@ -3,10 +3,11 @@
 [ "$1" ] && t="$1" || t='.'
 shift
 
-mount -B /dev "$t/dev"
-mount -B /sys "$t/sys"
-mount -B /proc "$t/proc"
-mount -B /dev/pts "$t/dev/pts"
+mount -o bind /dev "$t/dev"
+mount -o bind /dev/pts "$t/dev/pts"
+mount -t proc none "$t/proc"
+mount -t sysfs none "$t/sys"
+cp -L /etc/resolv.conf "$t/etc/resolv.conf"
 
 cp ${HOME}/.xauth* $t/root/ >/dev/null 2>&1
 cp ${HOME}/.Xauthority $t/root/ >/dev/null 2>&1
@@ -17,9 +18,9 @@ ln /tmp/.X11-unix/X0 $t/tmp/.X11-unix/X0  >/dev/null 2>&1
 chroot $t $*
 
 set -e
-umount "$t/dev/pts"
-umount "$t/proc"
 umount "$t/sys"
+umount "$t/proc"
+umount "$t/dev/pts"
 umount "$t/dev"
 
 exit 0
