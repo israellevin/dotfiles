4e92cb9019ab10f21d9f1c50ee298971b7f80d7b
I <root@bolt>
2017-03-11 12:36:33 +0200

    New status.sh, less kludgey.

--- bin/status.sh
+++ bin/status.sh
@@ -1,44 +1,14 @@
 #!/bin/dash
-
-pdate="$(date '+%a %d %b %y - %H:%M')"
-
-psens=$(sensors | grep °C | cut -d+ -f2 | cut -d ' ' -f1 | awk '{if(""==max){max=$1}; if($1>max){max=$1};} END {printf "%.0f", max}') 2> /dev/null
-if [ "$psens" != "0" ]; then
-    psens="$psens° "
-else
-    psens=''
-fi
-
-pvolm=$(amixer get Master | grep % | cut -d [ -f4 | cut -d ] -f1) 2> /dev/null
-if [ "$pvolm" != 'off' ]; then
-    pvolm="$(amixer get Master | grep -m 1 % | cut -d [ -f2 | cut -d ] -f1) " 2> /dev/null
-else
-    pvolm=''
-fi
-
-pplay=$(mpc status | grep "\[playing\]") 2> /dev/null
-if [ "$pplay" ]; then
-    pplay="$(mpc current -f '[%title%]|[%file%]' | sed -e 's|.*/||' -e 's|\....$||' -e 's|\(.\{1,40\}\).*|\1|;q').. "
-else
-    pplay=''
-fi
-
-pmail=$(grep -Po '(?<=<fullcount>).*(?=\</fullcount>)' ~/ars/root/unreadgmail.xml) 2> /dev/null
-
-if [ "$pmail" \> "0" ]; then
-    pmail="[$pmail] "
-else
-    pmail=''
-fi
-
-pbatt=$(acpi) 2> /dev/null
-if [ "$pbatt" ]; then
-    if [ D = $(echo $pbatt | cut -c 12) ]; then
-        pbatt="$(echo $pbatt | cut -d ' ' -f 5) "
-    else
-        pbatt="$(echo $pbatt | cut -d ',' -f 2 | sed 's/ /⌁/') "
-    fi
-fi
-
-echo -n "$pbatt$pmail$pplay$pvolm$psens$pdate"
+(
+    pdate="$(date '+%a %H:%M %F')"
+    pmail=$(grep -Po '(?<=<fullcount>).*(?=\</fullcount>)' ~/ars/root/unreadgmail.xml)
+    psens=$(sensors | grep -o '[[:digit:]]\{2\}\.[[:digit:]]' | sort -n | tail -1)°
+    pbatt=$(acpi | grep -o '[[:digit:]]\{2\}:[[:digit:]]\{2\}')
+    [ "$pbatt" ] && pbatt="⌁$pbatt" || pbatt=$(acpi | grep -o '[[:digit:]]*%')
+    pvolm=$(amixer get Master | grep -o '[[:digit:]]*%')
+    mpc status | grep "\[playing\]" > /dev/null && pplay="$(mpc current -f '[%title%]|[%file%]')"
+    for item in "$pdate" $pmail $pbatt $psens $pvolm $pplay; do
+        echo -n "$item "
+    done
+) 2> /dev/null | awk '{if (length($0) > 60) print substr($0, 1, 59) "…"; else{sub(/.$/, ""); print;}}'
 exit 0
