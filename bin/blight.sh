7b1fe7a1a18bde289862d2adf449b542ef585024
I <root@boltage>
2019-02-18 13:51:06 +0200

    WTF was that?

--- bin/blight.sh
+++ bin/blight.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-blight() {
-    echo $((
-        ${1:-100} * $(cat /sys/class/backlight/intel_backlight/max_brightness) / 100
-    )) > /sys/class/backlight/intel_backlight/brightness;
-}
-
-[ "${BASH_SOURCE[0]}" = "$0" ] && blight $1 || true
+echo $((
+    ${1:-100} * $(cat /sys/class/backlight/intel_backlight/max_brightness) / 100
+)) > /sys/class/backlight/intel_backlight/brightness;
