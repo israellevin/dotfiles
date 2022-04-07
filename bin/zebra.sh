51925ad52c813f532d548f502267cd2d699118c0
I <root@pod>
2022-04-07 12:41:58 +0300

    Red zebra first

--- bin/zebra.sh
+++ bin/zebra.sh
@@ -2,7 +2,7 @@
 REVERSE="$(tput rev)"
 CLEAR="\e[m"
 while read line; do
-    printf "%s\n" "$line"
-    read line
     printf "\x1b[48;5;%sm%s\e[0m\n" 52 "$line"
+    read line
+    printf "%s\n" "$line"
 done
