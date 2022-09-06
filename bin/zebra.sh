bc171d90c8ed3fc2bce6d65fd732319055d3c0cd
Israel Levin <israel@deweb.io>
2022-09-06 23:21:54 +0300

    Fix torst empty directory bug
    Also remove torst clean functionality, because webtorrent saves torrents
    in directories with readable names.
    
    Also clarify zebra.sh, and put a builtin version in torst.

--- bin/zebra.sh
+++ bin/zebra.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-REVERSE="$(tput rev)"
-CLEAR="\e[m"
+ALTERNATE=$'\001'"$(tput setab 1)"$'\002'
+RESET=$'\001'"$(tput sgr0)"$'\002'
 while read line; do
-    printf "\x1b[48;5;%sm%s\e[0m\n" 52 "$line"
+    echo "$ALTERNATE$line$RESET"
     read line
-    printf "%s\n" "$line"
+    echo "$line"
 done
