38876c192347e2336c78e298eb01041b3b4cdcaf
I. <root@chakra>
2024-02-01 10:00:56 +0200

    Ordered umounting on xchroot

--- bin/xchroot.sh
+++ bin/xchroot.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
 
 [ "$1" ] && target="$1" || target='.'
-shift
+shift || true
+target="$(readlink -f "$target")"
 mountpoints='dev dev/pts proc sys'
 
+# Sort and reverse sort mountpoints for mounting and unmounting.
+depth() { echo "$1" | grep -o '/' | wc -l; }
+depthsort() {
+    for mountpoint in $mountpoints; do
+        echo "$(depth "$mountpoint") $mountpoint"
+    done | sort -n | cut -d' ' -f2-
+}
+# newlines to spaces
+mountpoints="$(depthsort | sed ':a;N;$!ba;s/\n/ /g')"
+reversedmountpoints="$(depthsort | tac | sed ':a;N;$!ba;s/\n/ /g')"
+
+# Bind mountpoints.
 for mountpoint in $mountpoints; do
     [ -d "/$mountpoint" ] && [ -d "$target/$mountpoint" ] && mount -B /$mountpoint "$target/$mountpoint"
 done
 
-cp ${HOME}/.xauth* $target/root/ >/dev/null 2>&1
+## Copy X credentials.
 cp ${HOME}/.Xauthority $target/root/ >/dev/null 2>&1
 mkdir $target/tmp/.X11-unix/  >/dev/null 2>&1
 rm -f $target/tmp/.X11-unix/X0 2>&1
 ln /tmp/.X11-unix/X0 $target/tmp/.X11-unix/X0  >/dev/null 2>&1
 
+## Actual chroot.
 chroot "$target" "$@"
 
-for mountpoint in $mountpoints; do
-    echo "!!! - $mountpoint"
-    echo "mount | grep '$target/$mountpoint'"
-    mount | grep "$target/$mountpoint"
-    mount | grep "$target/$mountpoint" && umount "$target/$mountpoint"
+# Try to unmount any mounted mountpoints.
+for mountpoint in $reversedmountpoints; do
+    mount | grep -q "$target/$mountpoint" && umount "$target/$mountpoint"
 done
 
-for mountpoint in $mountpoints; do
+# Warn if any mountpoints is still mounted.
+for mountpoint in $reversedmountpoints; do
     mount | grep "$target/$mountpoint" && echo "$target/$mountpoint" still mounted
 done
 
