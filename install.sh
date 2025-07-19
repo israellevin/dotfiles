f55537554d6843a3dae53b5d084ac09c8b3600b8
I. <root@chakra>
2025-07-19 19:34:14 +0300

    Add python and nodejs envs and fix typo

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 if [ "$1" = --apt-install ]; then
     shift
     apt-get --no-install-recommends --no-install-suggests install \
-        bash-completion bc bsdextrautils curl git locales mc moreutils psmisc tmux unzip vim tmux vim wget
+        bash-completion bc bsdextrautils curl git locales mc moreutils npm psmisc python3-venv tmux unzip vim tmux vim wget
     echo en_US.UTF-8 UTF-8 > /etc/locale.gen
     locale-gen
 fi
 
 pushd "$(dirname "$(realpath "$0")")" || exit 1
-if git remote show -n origin 2>/dev/null | grep -q '^ *Fetch URL:.*israellevin/dotfiles\(.git\)*$'; then
+if ! git remote show -n origin 2>/dev/null | grep -q '^ *Fetch URL:.*israellevin/dotfiles\(.git\)*$'; then
     git clone https://israellevin@github.com/israellevin/dotfiles
     cd dotfiles
 fi
@@ -24,6 +24,14 @@ mv ./trans ./bin/.
 
 cp --preserve=all ./.* ~/.
 cp -a ./bin ./.config ~/.
+
+python3 -m venv ~/bin/python
+. ~/bin/python/bin/activate
+pip install --upgrade pip setuptools
+pip install pygments python-lsp-server shell-gpt
+
+npm --prefix ~/bin install npm@latest typescript-language-server webtorrent-cli
+rm ~/bin/package.json ~/bin/package-lock.json
 
 LC_ALL=en_US.UTF-8 vim +:qa
 [ -t 0 ] && . ~/.bashrc
