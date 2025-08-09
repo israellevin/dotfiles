27cd0caccf84f968628ab5d41cf8b6d5cfc0f5ea
I <root@raja>
2025-08-10 00:18:59 +0300

    Delete a bunch of unused stuff

--- bin/gmail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-gmail_login=""
-gmail_password=""
-while true
-do
-    wget --secure-protocol=TLSv1 --timeout=3 -t 1 -q -O - "https://${gmail_login}:${gmail_password}@mail.google.com/mail/feed/atom" --no-check-certificate > "$HOME/unreadgmail.xml"
-    sleep 666
-done
