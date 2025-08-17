367c11798c818cd8b32bd2954b459dce12165123
i <40337+israellevin@users.noreply.github.com>
2025-08-17 15:32:43 +0300

    Nicer diff script

--- diffwithhome.sh
+++ diffwithhome.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-for f in .* bin/*; do [ -f "$f" ] && [ "$(diff {./,~/}"$f")" ] && read -p "$f" && vimdiff {./,~/}"$f"; done
+for f in .* bin/*; do
+    [ -f "$f" ] || continue
+
+    if ! [ -f "$HOME/$f" ]; then
+        create=y
+        read -n1 -p "$f not found in home. Copy from repo? [Y/n] " create && echo
+        [ "$create" != n ] && cp -a "$f" "$HOME/$f" && echo "Created $HOME/.$f"
+        continue
+    fi
+
+    [ "$(diff {.,$HOME}/"$f")" ] || continue
+
+    show=y
+    read -n1 -p "$f differs from home. Show diff? [Y/n] " show && echo
+    [ "$show" != n ] && vimdiff {.,$HOME}/"$f"
+done
