0e3ea29680a4a5b55e5eb2bc68465f780bd67c6b
i <40337+israellevin@users.noreply.github.com>
2025-09-05 20:57:24 +0300

    Improved dwlauncher

--- bin/dwlaunch.sh
+++ bin/dwlaunch.sh
@@ -1,26 +1,31 @@
 #!/usr/bin/sh
+
+until [ -w /dev/dri/renderD128 ]; do :; done
+
 user_path="$HOME/bin:$HOME/bin/python/bin:$HOME/bin/cargo/bin:$HOME/bin/node/bin:$HOME/bin/node/node_modules/.bin"
 PATH="$user_path:$PATH:/sbin:$HOME/.fzf/bin"
 LANG=en_US.UTF-8
 EDITOR=vim
 BROWSER=brows
-LIBSEAT_BACKEND=seatd
-WLR_BACKENDS="libinput,drm"
-XDG_CURRENT_DESKTOP=dwl
 export PATH
 export LANG
 export EDITOR
 export BROWSER
+
+WAYLAND_DISPLAY=wayland-0
+XDG_CURRENT_DESKTOP=wlroots
+export WAYLAND_DISPLAY
+export XDG_CURRENT_DESKTOP
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+
+LIBSEAT_BACKEND=seatd
+WLR_BACKENDS="libinput,drm"
 export LIBSEAT_BACKEND
 export WLR_BACKENDS
-export XDG_CURRENT_DESKTOP
-
-until [ -w /dev/dri/renderD128 ]; do :; done
-
 exec dwl -s ' \
     red & \
     foot & \
     brows & \
     clip --start & \
-    /usr/libexec/xdg-desktop-portal-wlr & \
     wait'
