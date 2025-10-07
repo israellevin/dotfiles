710a577b6085493b941005fd86690b0b5eb9e927
I. <40337+israellevin@users.noreply.github.com>
2025-10-07 14:16:38 +0300

    Use logind for seat management

--- bin/dwlaunch.sh
+++ bin/dwlaunch.sh
@@ -7,22 +7,14 @@ PATH="$user_path:$PATH:/sbin:$HOME/.fzf/bin"
 LANG=en_US.UTF-8
 EDITOR=vim
 BROWSER=brows
-export PATH
-export LANG
-export EDITOR
-export BROWSER
-
 WAYLAND_DISPLAY=wayland-0
 XDG_CURRENT_DESKTOP=wlroots
-export WAYLAND_DISPLAY
-export XDG_CURRENT_DESKTOP
+WLR_BACKENDS=libinput,drm
+export PATH LANG EDITOR BROWSER WAYLAND_DISPLAY XDG_CURRENT_DESKTOP WLR_BACKENDS
+
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 
-LIBSEAT_BACKEND=seatd
-WLR_BACKENDS="libinput,drm"
-export LIBSEAT_BACKEND
-export WLR_BACKENDS
 exec dwl -s ' \
     red & \
     foot & \
