769549f104e1039b6033a41c58c4d0e246739e8e
I. <40337+israellevin@users.noreply.github.com>
2026-01-20 15:26:25 +0200

    Add fzf to wayland session launcher

--- bin/niri.sh
+++ bin/niri.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/sh
 until [ -w /dev/dri/renderD128 ]; do :; done
 user_path="$HOME/bin:$HOME/bin/python/bin:$HOME/bin/cargo/bin:$HOME/bin/node/node_modules/.bin"
-PATH="$user_path:$PATH:/sbin:$HOME/bin/node/bin:$HOME/.fzf/bin"
+PATH="$user_path:$PATH:/sbin"
 LANG=en_US.UTF-8
 EDITOR=vim
 BROWSER=brows
