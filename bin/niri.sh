bd6c289b7edb6f0a51e1c6dac5c0de239d925298
I. <40337+israellevin@users.noreply.github.com>
2026-01-25 03:07:34 +0200

    Launch niri as session

--- bin/niri.sh
+++ bin/niri.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/sh
 until [ -w /dev/dri/renderD128 ]; do :; done
-user_path="$HOME/bin:$HOME/bin/python/bin:$HOME/bin/cargo/bin:$HOME/bin/node/node_modules/.bin"
+user_path="$HOME/bin"
+user_path="$user_path:$HOME/bin/python/bin"
+user_path="$user_path:$HOME/bin/cargo/bin"
+user_path="$user_path:$HOME/bin/node/bin"
+user_path="$user_path:$HOME/bin/node/node_modules/.bin"
+user_path="$user_path:$HOME/.fzf/bin"
 PATH="$user_path:$PATH:/sbin"
 LANG=en_US.UTF-8
 EDITOR=vim
@@ -9,4 +14,4 @@ XDG_CONFIG_HOME="$HOME/.config"
 XDG_CACHE_HOME="$HOME/.cache"
 XDG_DATA_HOME="$HOME/.local/share"
 export PATH LANG EDITOR BROWSER XDG_CONFIG_HOME XDG_CACHE_HOME XDG_DATA_HOME
-exec niri
+exec niri --session
