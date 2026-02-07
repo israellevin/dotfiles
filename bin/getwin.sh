ae6626f16e60f15b19df70d3b1c70d0192ea0bbe
I. <40337+israellevin@users.noreply.github.com>
2026-02-07 13:15:33 +0200

    Add app ID to window selection menu

--- bin/getwin.sh
+++ bin/getwin.sh
@@ -3,7 +3,11 @@ if [ "$1" ]; then
     window_id="$(niri msg --json windows | jq -r ".[] | select(.title | test(\"$1\"; \"i\")) | .id")"
 fi
 if ! [ "$window_id" ]; then
-    window_id="$(niri msg --json windows | jq -r '.[] | "\(.id): \(.title)"' | menu | cut -d: -f1)"
+    window_id="$( \
+        niri msg --json windows | \
+        jq -r '.[] | "\(.id): \(.title) (\(.app_id))"' | \
+        menu | \
+        cut -d: -f1)"
 fi
 if [ "$window_id" ]; then
     niri msg action focus-window --id "$window_id"
