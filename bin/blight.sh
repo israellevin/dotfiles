f9303870c9b1bee4d90d647333a1d45f17736ec0
I. <40337+israellevin@users.noreply.github.com>
2026-04-02 14:14:20 +0300

    Add keyboard backlight support for blight.sh

--- bin/blight.sh
+++ bin/blight.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 maxfile=/sys/class/backlight/intel_backlight/max_brightness
 valfile=/sys/class/backlight/intel_backlight/brightness
+kbdfile='/sys/class/leds/tpacpi::kbd_backlight/brightness'
+
 maxlight=$(cat $maxfile)
+
+if [ "$2" -eq "$2" ] 2> /dev/null; then
+    kbdlight=$2
+else
+    kbdlight=0
+fi
 
 if [ "$1" -eq "$1" ] 2> /dev/null; then
     centilight=$1
@@ -11,11 +19,16 @@ else
         centilight=$(echo "$curlight + 10" | bc)
     elif [ "$1" = "down" ]; then
         centilight=$(echo "$curlight - 10" | bc)
+
+    # For default action, toggle between 20% and 50% brightness and kyeboard backlight.
+    elif [ "$curlight" -gt 30 ]; then
+            centilight=20
+            kbdlight=1
     else
-        [ "$curlight" -gt 30 ]&& centilight=20 || centilight=50
+        centilight=50
+        kbdlight=0
     fi
 fi
 
-newlight=$(echo "$maxlight / 100 * $centilight" | bc)
-
-echo "$newlight" | su -c "tee '$valfile'"
+echo "$maxlight / 100 * $centilight" | bc | su -c "tee '$valfile'"
+echo "$kbdlight" | su -c "tee '$kbdfile'"
